70c52b6a341ca4a687c0b56bf96a96636b795ed4
farrance <michael.farrance@grandvisual.com>
2014-03-31 15:08:20 +0100

    Functional Version
    Finished, has clock, fade In animations and error handling for when the
    API is down.

---
+++ class/class.signage.php
@@ -0,0 +1,64 @@
+<?php
+class Signage {
+	
+	//Function for getting Tube Status as HTML
+	public function TubeStatus(){
+		
+		//Inital HTML response
+		$html = '';
+		
+		//Load tube status data
+		$xml = @simplexml_load_file('http://cloud.tfl.gov.uk/TrackerNet/LineStatus');
+		
+		if($xml){
+		
+			$tubes = array();
+			
+			//Push tube names, colours and statuses into an array
+			foreach($xml->LineStatus as $line){
+				$tubes[] = array(
+					"name" => $line->Line['Name'],
+					"class" => strtolower(preg_replace("/[^A-Za-z0-9]/", "", $line->Line['Name'])),
+					"status" => $line->Status['Description']
+				);
+			}
+			
+			//Return array in HTML format
+			foreach($tubes as $tube){
+				
+				$name	= $tube['name'];
+				$class	= $tube['class'];
+				$status	= $tube['status'];
+				
+				$html.= "		<div class=\"$class tube-animate\">";
+				$html.= "			<div class=\"tube-badge\">";
+				$html.= "				<p>$name</p>";
+				$html.= "			</div>";
+				$html.= "			<div class=\"tube-info\">";
+				$html.= "				<p>$status</p>";
+				$html.= "			</div>";
+				$html.= "		</div>";
+				
+			}
+			
+		}
+		
+		//IF API is down, return pretty error
+		else {
+		
+			$html.= "		<div class=\"tube-error tube-animate\">";
+			$html.= "			<div class=\"tube-badge $class\">";
+			$html.= "				<p>Failed</p>";
+			$html.= "			</div>";
+			$html.= "			<div class=\"tube-info\">";
+			$html.= "				<p>Tube API is down.</p>";
+			$html.= "			</div>";
+			$html.= "		</div>";
+			
+		}
+		
+		return $html;
+	}
+		
+}
+?>
--- index.php
+++ index.php
@@ -1,14 +1,56 @@
+<?php
+require_once('class/class.signage.php');
+$Signage = new Signage();
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link href="css/style.css" rel="stylesheet" />
 <title>GV Signage</title>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 </head>
-
 <body>
-<div id="wrapper">
-
+<div class="wrapper">
+	<div class="header">
+		<img src="img/logo.png"  />
+	</div>
+	<div class="hero">
+		<h1>Live Tube Statuses</h1>
+	</div>
+	<div class="tube">
+		<?php echo $Signage->TubeStatus(); ?>
+	</div>
+	<div class="clock"></div>
 </div>
+<script>
+
+//Clock
+function clock() {
+	
+	var date	= new Date();
+	var hour	= date.getHours();
+	var minute	= date.getMinutes();
+	var second	= date.getSeconds();
+	var time	= hour + ':' + minute + ':' + second;
+	
+   $('.clock').html(time);
+   setTimeout('clock()',1000);
+   
+}
+clock();
+
+//Animate all the tube info one at a time
+function animateIn(){
+	$('.tube-animate').each(function (i) {
+		$(this)
+		.delay( 200*i )
+		.css( { opacity : 0, "z-index": 1 } )
+		.animate( { opacity: 1 }, 500 );
+	});
+}
+
+animateIn();
+</script>
 </body>
 </html>
